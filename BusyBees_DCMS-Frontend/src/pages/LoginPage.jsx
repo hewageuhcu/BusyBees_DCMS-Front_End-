fe3c02ba3ed1bff048d16bf34cc9d7edff7f3995
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-10-17 05:03:50 +0900

    Refactor login page and add sidebar styling

--- BusyBees_DCMS-Frontend/src/pages/LoginPage.jsx
+++ BusyBees_DCMS-Frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,9 @@
 // src/LoginPage.jsx
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
+import ChildCareIcon from '@mui/icons-material/ChildCare';
+import MenuIcon from '@mui/icons-material/Menu'; // Add this import
+import { Toolbar, Typography, IconButton } from '@mui/material';
 
 
 const LoginPage = () => {
@@ -9,13 +11,33 @@ const LoginPage = () => {
 
   const onSubmit = (data) => {
     console.log(data);
-    // Handle the login logic (e.g., API call)
+    // Handle login logic (e.g., API call)
   };
 
   return (
     <div className="container">
       {/* Sidebar */}
-   <div className="sidebar"></div>
+      <div className="sidebar">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          
+          </IconButton>
+          <ChildCareIcon sx={{ color: '#FFFFFF', fontSize: 40, mr: 1 }} />
+          <Typography
+            variant="h4"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, color: '#FFFFFF', fontWeight: 'bold',width:40 }}
+          >
+            BusyBees
+          </Typography>
+        </Toolbar>
+      </div>
 
       {/* Login Form */}
       <div className="login-form">
@@ -26,7 +48,7 @@ const LoginPage = () => {
             <input
               type="email"
               id="email"
-              {...register("email", { required: "Email is required" })}
+              {...register('email', { required: 'Email is required' })}
             />
             {errors.email && <span>{errors.email.message}</span>}
           </div>
@@ -36,7 +58,7 @@ const LoginPage = () => {
             <input
               type="password"
               id="password"
-              {...register("password", { required: "Password is required" })}
+              {...register('password', { required: 'Password is required' })}
             />
             {errors.password && <span>{errors.password.message}</span>}
           </div>
